e2b2ea3b91047e5633a68f8912d6346e5de8e418
silvina <silvina.busi@gmail.com>
2023-02-05 20:10:04 -0300

    agregue codigo js y local storage

--- js/script2.js
+++ js/script2.js
@@ -3,58 +3,66 @@ const productos = [
         id: 1,
         titulo: "Mamaderas",
         imagen: "./img/MAMADERA.jpg",
-        precio: 800
+        precio: 800,
+        cantidad: 1,
     },
     {
         id: 2,
         titulo: "Vasos",
         imagen: "./img/VASOS.jpg",
-        precio: 750
+        precio: 750,
+        cantidad: 1,
     },
     {
         id: 3,
         titulo: "Chupetes",
         imagen: "./img/CHUPETES.png",
-        precio: 1200
+        precio: 1200,
+        cantidad: 1,
     },
     {
         id: 4,
         titulo: "Platos",
         imagen: "./img/PLATOS.jpg",
-        precio: 1000
+        precio: 1000,
+        cantidad: 1,
     },
     {
         id: 5,
         titulo: "Cubiertos",
         imagen: "./img/CUBIERTOS.jpg",
-        precio: 500
+        precio: 500,
+        cantidad: 1,
     },
     {
         id: 6,
         titulo: "Baberos",
         imagen: "./img/BABEROS.jpg",
-        precio: 400
+        precio: 400,
+        cantidad: 1,
     },
     {
         id: 7,
         titulo: "Portachupetes",
         imagen: "./img/PORTACHUPETES.jpeg",
-        precio: 300
+        precio: 300,
+        cantidad: 1,
     },
     {
         id: 8,
         titulo: "Sonajero",
         imagen: "./img/SONAJERO.jpg",
-        precio: 800
+        precio: 800,
+        cantidad: 1,
     }
 ]
 
 const contenedorProductos = document.getElementById("contenedor-productos")
 const carritoCompras = document.getElementById("carritoCompras")
 const contenedorCarrito = document.getElementById("contenedor-carrito")
+const cantidadCarrito = document.getElementById("cantidadCarrito")
 
-
-let carrito = []
+let carrito = JSON.parse(localStorage.getItem("carrito")) || []
 
 productos.forEach((producto) => {
     let content = document.createElement("div")
@@ -73,17 +81,33 @@ productos.forEach((producto) => {
     content.append(comprar)
 
     comprar.addEventListener("click", () => {
-        carrito.push({
-            id: producto.id,
-            imagen: producto.imagen,
-            titulo: producto.titulo,
-            precio: producto.precio
-        })
+        const repeat = carrito.some((repeatProduct) => repeatProduct.id === producto.id)
+
+        if (repeat) {
+            carrito.map((product) => {
+                if (product.id === producto.id) {
+                    product.cantidad++
+                }
+            })
+        } else {
+
+            carrito.push({
+                id: producto.id,
+                imagen: producto.imagen,
+                titulo: producto.titulo,
+                precio: producto.precio,
+                cantidad: producto.cantidad
+            })
+        }
         console.log(carrito)
+        carritoContador()
+        saveLocal()
     })
 })
 
-carritoCompras.addEventListener("click", () => {
+const pintarCarrito = () => {
+    contenedorCarrito.innerHTML = ""
+    contenedorCarrito.style.display = "flex"
     const carritoHeader = document.createElement("div")
     carritoHeader.className = "carrito-header"
     carritoHeader.innerHTML = `
@@ -92,8 +116,11 @@ carritoCompras.addEventListener("click", () => {
     contenedorCarrito.append(carritoHeader)
 
     const carritoBoton = document.createElement("h1")
-    carritoBoton.innerText = "X"
+    carritoBoton.innerText = "vaciar carrito"
     carritoBoton.className = "carrito-boton"
+    carritoBoton.addEventListener("click", () => {
+        contenedorCarrito.style.display = "none"
+    })
 
     carritoHeader.append(carritoBoton)
 
@@ -101,20 +128,57 @@ carritoCompras.addEventListener("click", () => {
         let carritoContent = document.createElement("div")
         carritoContent.className = "carrito-content"
         carritoContent.innerHTML = `
-    <img src="${producto.imagen}">
-    <h3>${producto.titulo}</h3>
-    <p>${producto.precio}
+    <img class= "img-carrito" src="${producto.imagen}">
+    <h3 class= "titulo-carrito">${producto.titulo}</h3>
+    <p class= "p-carrito">$ ${producto.precio}</p>
+    <p class = "p-carrito">Cantidad: ${producto.cantidad}</p>
+    <span class= "delete-producto"> ❌ </span>
 
     `
         contenedorCarrito.append(carritoContent)
+        console.log(carrito.length)
+
+        let eliminar = carritoContent.querySelector(".delete-producto")
+
+        eliminar.addEventListener("click", () =>{
+            eliminarProducto(producto.id)
+        })
+
     })
 
     const total = carrito.reduce((acc, el) => acc + el.precio, 0)
 
     const totalCompra = document.createElement("div")
     totalCompra.className = "total-compra"
-    totalCompra.innerHTML = `total a pagar: ${total} $`
+    totalCompra.innerHTML = `Total a pagar: $${total}`
 
     contenedorCarrito.append(totalCompra)
+}
 
-})
+carritoCompras.addEventListener("click", pintarCarrito)
+
+const eliminarProducto = (id) => {
+    const foundId = carrito.find((element) => element.id === id)
+    carrito = carrito.filter((carritoId) => {
+        return carritoId !== foundId
+    })
+    carritoContador()
+    saveLocal()
+    pintarCarrito()
+}
+
+const carritoContador = () => {
+    cantidadCarrito.style.display = "block"
+
+    const carritoLength = carrito.length
+    localStorage.setItem("carritoLength", JSON.stringify(carritoLength))
+    cantidadCarrito.innerText = JSON.parse(localStorage.getItem("carritoLength"))
+}
+
+carritoContador()
+
+/* LOCAL STORAGE */
+const saveLocal = () => {
+localStorage.setItem("carrito", JSON.stringify(carrito))
+}
+
